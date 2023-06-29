b8b87202c988d2de18377a14c0f2b261e2b0e821
tzanchi <tzanchi@student.42berlin.de>
2023-06-29 18:31:22 +0200

    correction

--- sources/main.c
+++ sources/main.c
@@ -6,15 +6,18 @@
 /*   By: tzanchi <tzanchi@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 15:30:04 by tzanchi           #+#    #+#             */
-/*   Updated: 2023/06/29 11:24:38 by tzanchi          ###   ########.fr       */
+/*   Updated: 2023/06/29 18:21:01 by tzanchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	init_stack_a(t_stack **a, char **argv)
+/*Initialize stack with the parameter string in a list. Each node is composed
+of an integer, its index in the list and a pointer to next node
+Returns the length of the list*/
+size_t	init_stack_a(t_stack **a, char **argv)
 {
-	int	index;
+	size_t	index;
 
 	index = 0;
 	while (*argv)
@@ -26,26 +29,27 @@ void	init_stack_a(t_stack **a, char **argv)
 		a = &((*a)->next);
 	}
 	*a = NULL;
+	return (index);
 }
 
 int	main(int argc, char **argv)
 {
 	t_stack	*a;
 	t_stack	*b;
+	size_t	size;
 
-	if (argc < 2 || !input_is_valid(argv))
+	if (argc < 2 || !input_is_valid(argv + 1))
 	{
 		ft_printf("%s", ERROR_MESSAGE);
 		return (1);
 	}
 	a = NULL;
 	b = malloc(sizeof(t_stack));
-	init_stack_a(&a, argv + 1);
-	while (a && b)
+	size = init_stack_a(&a, argv + 1);
+	while (a)
 	{
-		ft_printf("%d, %d    %d, %d\n", a->value, a->index, b->value, b->index);
+		pb(&a, &b);
 		a = a->next;
 	}
-	ft_printf("----    ----\n a       b");
 	return (0);
 }
